016c03b30a9193b8b7da0a9ae67d2e6e4ed91ea2
يحيى حمود <fantasy-art@live.com>
2023-10-13 15:40:09 +0300

    ch5

--- lib/ios_devices/mixin/iphone14.dart
+++ lib/ios_devices/mixin/iphone14.dart
@@ -5,6 +5,7 @@ import '../screen.dart';
 
 class Iphon14 extends Devices with ScreenSize, Camera, Chipset14 {
   Iphon14({required super.devName, required super.devPrice});
+
   void printFeature() {
     print(
         'Name Device: ${this.devName}, Price:${this.devPrice} SR\nFeature:\nChipset:${this.chipset14()}\nScreen:${this.screenSize()}\nCamera:${this.camera14()}');
